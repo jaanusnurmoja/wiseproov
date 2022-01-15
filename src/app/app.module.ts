f39ed9f947df899e307a709aba8264b6caa1189e
Jaanus Nurmoja <jaanus.nurmoja@gmail.com>
2022-01-15 15:26:30 +0200

    komponendid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+import { TableComponent } from './content/table/table.component';
+import { ArticleComponent } from './content/article/article.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    TableComponent,
+    ArticleComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
