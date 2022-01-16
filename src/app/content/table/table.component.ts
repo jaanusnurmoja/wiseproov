0f22d86751b49bb0e2dfff5c0c281454dcd43d8a
Jaanus Nurmoja <jaanus.nurmoja@gmail.com>
2022-01-16 23:58:18 +0200

    Hetkeseis: olemas loetelu inimestest ja leheküljenumbrite loend (linkideta veel), üksikartikkel on praegu lihtsalt silmailu pärast copypaste'itud. Muudetud ka tsconfig.json, et "implicitly has an 'any' type" veateade tööd ei takistaks :)

--- src/app/content/table/table.component.ts
+++ src/app/content/table/table.component.ts
@@ -7,9 +7,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableComponent implements OnInit {
 
+  loendiStat: any;
+  inimesteLoend: any;
+  offsets: any;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.loendJaStatOrig();
+    //this.loendJaStatParsed();
+    //this.inimesed();
   }
 
+  loendJaStatOrig(os = 0): void {
+    fetch('https://midaiganes.irw.ee/api/list?offset='+os)
+    .then(response => response.json())
+    .then(j => this.loendJaStatParsed(j));
+  }
+
+loendJaStatParsed(j): void {
+  let loendiStat = j.stats;
+  let inimesteLoend = j.list;
+    this.loendiStat = loendiStat;
+    this.inimesteLoend = inimesteLoend;
+  let pages = loendiStat.total / loendiStat.limit;
+  let offsets:{page: number, value: number}[] = [];
+    for (let i = 0; i < pages; i++) {
+      offsets[i] = {"page": i+1, "value": i * loendiStat.limit};
+    }
+  this.offsets = offsets;
+  console.log(this.inimesteLoend);
+  console.log(this.loendiStat);
+  console.log(this.offsets);
+  }
+/*
+  inimesed(sortparams=[]): void {
+    let inimesed = this.loendJaStat.list;
+    let field = '';
+    let direction = "";
+    if (sortparams == []) {
+      field = 'surname';
+      direction = "asc";
+    }
+    else {
+      field = sortparams[0];
+      direction = sortparams[1];
+    }
+    inimesed.sort(sortData(field, direction));
+    this.inimesteLoend = inimesed;
+  }
+ */
+/*
+  tabeliLehed(loendiStat): void {
+    let pages = loendiStat.total / loendiStat.limit;
+    let offsets:number[];
+    for (let i = 0; i < pages; i++) {
+    offsets[i] = i * loendiStat.limit;
+    }
+    this.offsets = offsets;
+    console.log(this.offsets);
+  } */
 }
