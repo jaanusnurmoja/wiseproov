eaf365973e772d8de63d35b3683f79c5ba9226b4
Jaanus Nurmoja <jaanus.nurmoja@gmail.com>
2022-01-22 17:37:06 +0200

    Artikli objekt on toodud komponenti. Detailivaade veel hardcoded, debug elemendiks on inimese nimi.

--- src/app/content/article/article.component.ts
+++ src/app/content/article/article.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-article',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticleComponent implements OnInit {
 
-  constructor() { }
+  article: any;
+  id: any;
+
+  constructor(private _Activatedroute:ActivatedRoute) { }
 
   ngOnInit(): void {
+    this._Activatedroute.paramMap.subscribe(params => {
+        this.id = params.get('id') || '4e34a5c4';
+        this.getArticleFromUrl(this.id);
+    });
   }
 
+  getArticleFromUrl(id): void {
+    fetch('https://midaiganes.irw.ee/api/list/' + id)
+    .then(response => response.json())
+    .then(a => this.setArticle(a));
+  }
+
+  setArticle(a) : void {
+    this.article = a;
+  }
 }
