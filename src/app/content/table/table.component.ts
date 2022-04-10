4912ec467ec7f164f5649086c6f5a7aeb441e66e
Jaanus Nurmoja <jaanus.nurmoja@gmail.com>
2022-04-10 04:41:54 +0300

    nupula koodi htmlis kärbitud, ts-i lisatud navigate() ja nupulaKlass()
    et ma seda võimalust ka kohe tähele ei pannud :D

--- src/app/content/table/table.component.ts
+++ src/app/content/table/table.component.ts
@@ -4,16 +4,14 @@ import { parsePhoneNumber } from 'libphonenumber-js';
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.css']
+  styleUrls: ['./table.component.css'],
 })
-
 export class TableComponent implements OnInit {
-
   loendiStat: any;
   inimesteLoend: any;
   sliceInimesed: any;
   total: number = 0;
-  limit:number = 10;
+  limit: number = 10;
   start = 0;
   next = this.limit;
   pageTotal: any;
@@ -27,18 +25,18 @@ export class TableComponent implements OnInit {
   sortableField: any;
   sortToggleName: any;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
     this.getTotal();
     this.setSortableFieldNames();
-    this.setSortToggleNameAndSort('default','sort');
+    this.setSortToggleNameAndSort('default', 'sort');
   }
 
   getTotal(): void {
     fetch('https://midaiganes.irw.ee/api/list?limit=0')
-    .then(r => r.json())
-    .then(t => this.setTotal(t));
+      .then((r) => r.json())
+      .then((t) => this.setTotal(t));
   }
 
   setTotal(t): void {
@@ -54,20 +52,18 @@ export class TableComponent implements OnInit {
 
   loendJaStatOrig(total, param = ''): void {
     fetch('https://midaiganes.irw.ee/api/list?limit=' + total + param)
-    .then(response => response.json())
-    .then(j => this.loendJaStatParsed(j));
+      .then((response) => response.json())
+      .then((j) => this.loendJaStatParsed(j));
   }
 
-
   loendJaStatParsed(j): void {
-
     let loendiStat = j.stats;
     let inimesteLoend = j.list;
 
     for (let inimene of inimesteLoend) {
       inimene.firstname = inimene.firstname.replace('\u200e', '');
       inimene.surname = inimene.surname.replace('\u200e', '');
-      let sugu:string = '';
+      let sugu: string = '';
       sugu = inimene.sex == 'm' ? 'Mees' : 'Naine';
       inimene.sex = sugu;
       this.personalIdToFormattedDate(inimene.personal_code);
@@ -84,12 +80,17 @@ export class TableComponent implements OnInit {
     this.pageTotal = pages;
     this.lastPageIndex = this.pageTotal - 1;
 
-    let offsets:{pageIndex: number, page: number, value: number, next: number}[] = [];
+    let offsets: {
+      pageIndex: number;
+      page: number;
+      value: number;
+      next: number;
+    }[] = [];
 
     for (let i = 0; i < pages; i++) {
       let osValue = i * this.limit;
       let next = osValue + this.limit;
-      offsets[i] = {"pageIndex": i,"page": i+1, "value": osValue, "next": next};
+      offsets[i] = { pageIndex: i, page: i + 1, value: osValue, next: next };
     }
     this.offsets = offsets;
     this.firstOffset = offsets[0];
@@ -97,17 +98,17 @@ export class TableComponent implements OnInit {
 
   setSortToggleNameAndSort(sortableField, toggleName): void {
     let loend = this.inimesteLoend;
-    let sortNames:any = {};
-    let sortedData:any[] = [];
-    let finalSort:any[] = [];
-    let asc:boolean = false;
-    let desc:boolean = false;
-    let none:boolean = false;
-    let sn:string = 'sort';
-    if (toggleName =='') {
+    let sortNames: any = {};
+    let sortedData: any[] = [];
+    let finalSort: any[] = [];
+    let asc: boolean = false;
+    let desc: boolean = false;
+    let none: boolean = false;
+    let sn: string = 'sort';
+    if (toggleName == '') {
       sn = 'sort';
     }
-    if (toggleName =='sort') {
+    if (toggleName == 'sort') {
       sn = 'sort-up';
       asc = true;
     }
@@ -121,63 +122,98 @@ export class TableComponent implements OnInit {
     }
 
     if (sortableField == 'default') sortNames.default = 'sort';
-    if (sortableField == 'firstname'){
-        sortNames.firstname = sn;
-        sortedData = loend.sort((a, b) => (this.sortCompare(a.firstname, b.firstname, asc, desc)));
+    if (sortableField == 'firstname') {
+      sortNames.firstname = sn;
+      sortedData = loend.sort((a, b) =>
+        this.sortCompare(a.firstname, b.firstname, asc, desc)
+      );
     }
     if (sortableField == 'surname') {
       sortNames.surname = sn;
-      sortedData = loend.sort((a, b) => (this.sortCompare(a.surname, b.surname, asc, desc)));
+      sortedData = loend.sort((a, b) =>
+        this.sortCompare(a.surname, b.surname, asc, desc)
+      );
     }
     if (sortableField == 'sex') {
       sortNames.sex = sn;
-      sortedData = loend.sort((a, b) => (this.sortCompare(a.sex, b.sex, asc, desc)));
+      sortedData = loend.sort((a, b) =>
+        this.sortCompare(a.sex, b.sex, asc, desc)
+      );
     }
     if (sortableField == 'birthdate') {
       sortNames.sortCode = sn;
-      sortedData = loend.sort((a, b) => (this.sortCompare(a.sortCode, b.sortCode, asc, desc)));
+      sortedData = loend.sort((a, b) =>
+        this.sortCompare(a.sortCode, b.sortCode, asc, desc)
+      );
     }
     if (sortableField == '') {
-        sortNames.default = 'sort';
-        sortNames.firstname = 'sort';
-        sortNames.surname = 'sort';
-        sortNames.sex = 'sort';
-        sortNames.sortCode = 'sort';
+      sortNames.default = 'sort';
+      sortNames.firstname = 'sort';
+      sortNames.surname = 'sort';
+      sortNames.sex = 'sort';
+      sortNames.sortCode = 'sort';
     }
 
     this.sortToggleName = sortNames;
     if (none) {
-        this.reset();
-      } else {
+      this.reset();
+    } else {
       this.inimesteLoend = sortedData;
     }
-      this.setSliceInimesed(this.inimesteLoend, this.start, this.next);
+    this.setSliceInimesed(this.inimesteLoend, this.start, this.next);
   }
 
-  setSliceInimesed(inimesed = this.inimesteLoend, start?:number, next?:number): void {
+  navigate(target: string, pageIndex?: any, start?: any, next?: any) {
+    if (target == 'eelmine' || target == 'järgmine' || target == 'viimane') {
+      if (target == 'eelmine') {
+        pageIndex =
+          this.pageIndex - 1 < 0
+            ? this.pageTotal + (this.pageIndex - 1)
+            : this.pageIndex - 1;
+      }
+      if (target == 'järgmine') {
+        pageIndex =
+          this.pageIndex + 1 >= this.pageTotal
+            ? this.pageIndex + 1 - this.pageTotal
+            : pageIndex + 1;
+      }
+      start = this.offsets[pageIndex].value;
+      next = this.offsets[pageIndex].next;
+    }
+    this.pageIndex = pageIndex;
+    this.start = start;
+    this.next = next;
+    this.setSliceInimesed(this.inimesteLoend, this.start, this.next);
+  }
+
+  setSliceInimesed(
+    inimesed = this.inimesteLoend,
+    start?: number,
+    next?: number
+  ): void {
     if (typeof start === 'undefined') start = 0;
     if (typeof next === 'undefined') next = this.limit;
     this.start = start;
     this.next = next;
-    this.sliceInimesed = inimesed.slice(start,next);
+    this.sliceInimesed = inimesed.slice(start, next);
   }
 
   activeTr(id): void {
     this.isActive = this.isActive == id ? false : id;
   }
 
-  personalIdToFormattedDate(personalId):void {
+  personalIdToFormattedDate(personalId): void {
     let idAsString = String(personalId);
-    let sajandiArv = ['5','6'].includes(idAsString[0]) ? '20' : '19';
-    let aasta = sajandiArv + idAsString.substring(1,3);
-    this.sortSynna = Number(aasta+idAsString.substring(3));
-    let kuu = idAsString.substring(3,5);
-    let paev = idAsString.substring(5,7);
+    let sajandiArv = ['5', '6'].includes(idAsString[0]) ? '20' : '19';
+    let aasta = sajandiArv + idAsString.substring(1, 3);
+    this.sortSynna = Number(aasta + idAsString.substring(3));
+    let kuu = idAsString.substring(3, 5);
+    let paev = idAsString.substring(5, 7);
     this.synna = paev + '.' + kuu + '.' + aasta;
   }
 
   setSortableFieldNames(): void {
-    const field:any = {};
+    const field: any = {};
     field.firstname = 'firstname';
     field.surname = 'surname';
     field.sex = 'sex';
@@ -197,16 +233,37 @@ export class TableComponent implements OnInit {
     return 0;
   }
 
-  nupula (offset): boolean {
+  nupula(offset): boolean {
     if (this.pageIndex < 5) {
-      return offset.value >= 0 && offset.value < 10*this.limit;
+      return offset.value >= 0 && offset.value < 10 * this.limit;
+    } else if (this.pageIndex >= this.pageTotal - 5) {
+      return (
+        offset.value >= (this.pageTotal - 10) * this.limit &&
+        offset.value < this.total
+      );
+    } else {
+      return (
+        offset.value >= this.start - 5 * this.limit &&
+        offset.value < this.start + 5 * this.limit
+      );
     }
-    else if (this.pageIndex >= this.pageTotal - 5) {
-       return offset.value >= (this.pageTotal - 10) * this.limit && offset.value < this.total;
-    }
-    else {
-      return offset.value >= this.start-(5*this.limit) && offset.value < this.start+(5*this.limit);
-    }
+  }
+
+  nupulaKlass(offset) {
+    return offset.pageIndex == this.pageIndex.toString() ||
+      (!this.pageIndex && offset.pageIndex == '0')
+      ? {
+          'text-warning': true,
+          'fw-bold': true,
+          'fs-4': true,
+          'text-light': false,
+        }
+      : {
+          'btn-warning': false,
+          'fw-bold': false,
+          'fs-4': false,
+          'text-light': true,
+        };
   }
 
   reset(): void {
